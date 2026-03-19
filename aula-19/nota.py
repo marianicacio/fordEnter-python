27e93e48787a3ea47bcbe559ed278323b6d6ba49
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2026-03-19 11:53:02 -0300

    feat: first python class

---
+++ aula-19/nota.py
@@ -0,0 +1,15 @@
+nota1 = 6.5
+nota2 = 5
+nota3 = 5
+nota4 = 5
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+if(media >= 7):
+    print(f"Passou de ano: {media}")
+elif(media <=5):
+    print(f"Recuperação: {media}")
+else:
+    print(f"Reprovado: {media}")
+
+
