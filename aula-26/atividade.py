64f56c1c4d183697b2fd2ac0916d6cef26818218
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2026-03-27 09:08:58 -0300

    feat: atividade convite

--- aula-26/atividade.py
+++ aula-26/atividade.py
@@ -1,15 +1,34 @@
 pessoas = 0
 qtd_pessoas = 0
+bloqueadas = 0
+entradasTotal = 0
+entradasS = 0
 pessoas = int(input("Quantas pessoas vão entrar no evento: "))
 
 while pessoas > 0:
+    if(pessoas == qtd_pessoas):
+        break
     idade = int(input("idade: "))
-    if (idade >= 18):
-         print("Entrou")
-    elif(idade < 18 and idade >= 16):
-         convite = input("Essa pessoa possui convite: ")
-    elif(convite == "sim"):
-         print("Entrou com sucesso")
+    if(idade >= 18):
+        print("Passou")
+        entradasS += 1
+        entradasTotal += 1
+    elif(idade <= 17 and idade >= 16):
+        convite = input("Tem convite: ")
+        if(convite == "sim"):
+            print("Passou")
+            entradasTotal += 1
+        elif(convite == "não"):
+            print("Bloqueado")
+            bloqueadas += 1
+        else:
+            print("Inválido")
     else:
-         print("Bloqueado")
-    qtd_pessoas = pessoas =+ 1
+        print("Bloqueado") 
+        bloqueadas += 1       
+    qtd_pessoas += 1
+
+print(f"Entraram: {entradasTotal}")
+print(f"Entraram sem convite: {entradasS}")
+print(f"Pessoas barradas: {bloqueadas}")
+    
