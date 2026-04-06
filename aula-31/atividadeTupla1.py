b4a2617e9b33e3a0fa47c90d736f928d0ab081d6
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2026-04-06 17:05:59 -0300

    feat: atividade 1

---
+++ aula-31/atividadeTupla1.py
@@ -0,0 +1,7 @@
+# 1.Crie uma tupla com 4 números inteiros e exiba o segundo número.
+
+numeroInteiros = (1, 2, 3, 4 )
+
+numero = numeroInteiros[1]
+
+print(numero)
---
+++ aula-31/atividadeTupla2.py
@@ -0,0 +1,10 @@
+# 2.Crie duas tuplas com diferentes tipos de dados (ex: números e strings). Imprima o index 2 da tupla de números e o index 3 da tupla de string no mesmo print.
+
+numeros = (1, 2, 32, 21)
+palavras = ('hello world', 'maria', 'murillo', 'amor')
+
+num = numeros[2]
+pal = palavras[3]
+
+print(num)
+print(pal)
